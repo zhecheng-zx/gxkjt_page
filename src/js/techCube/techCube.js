62933fa4e96509706bede32e2b1cdd905554a956
lifei <359952175@qq.com>
2017-09-08 17:31:43 +0800

    1

--- src/js/techCube/techCube.js
+++ src/js/techCube/techCube.js
@@ -79,7 +79,16 @@ $(function () {
     chartTwo.setOption(option, true);
 
 
-    $('#datePicker').datetimepicker();
+    $('#datePicker').datetimepicker({
+        language:  'zh-CN',
+        weekStart: 1,
+        todayBtn:  1,
+        autoclose: 1,
+        todayHighlight: 1,
+        startView: 2,
+        minView: 2,
+        forceParse: 0
+    });
 });
 
 function  getTrendChart() {
