024c22a9baed4b2bcd79584ae28e07e7c819a380
lifei <359952175@qq.com>
2017-10-10 20:20:42 +0800

    企事业单位数据展示页面

---
+++ src/js/techPool/enterpriseDataDisplay.js
@@ -0,0 +1,54 @@
+$(function () {
+    var data1 = [];
+    for(var j = 0; j < 10; j++){
+        data1.push(j*10);
+    }
+
+    var data = [];
+
+    for (var i = 0; i < 10; i++) {
+        data.push(Math.random() * 100);
+    }
+
+    var option = {
+        tooltip: {
+            trigger: 'axis',
+            position: function (pt) {
+                return [pt[0], '10%'];
+            }
+        },
+        xAxis: {
+            type: 'category',
+            boundaryGap: false,
+            data: data1,
+            show: false
+        },
+        yAxis: {
+            type: 'value',
+            boundaryGap: [0, '100%'],
+            show: false
+        },
+        series: [
+            {
+                name:'分数',
+                type:'line',
+                smooth:true,
+                symbol: 'none',
+                sampling: 'average',
+                itemStyle: {
+                    normal: {
+                        color: 'rgb(0, 228, 148)'
+                    }
+                },
+                areaStyle: {
+                    normal: {
+                        color: 'rgb(238, 255, 249)'
+                    }
+                },
+                data: data
+            }
+        ]
+    };
+    var chart = echarts.init(document.getElementById('chart_grade'));
+    chart.setOption(option);
+});
