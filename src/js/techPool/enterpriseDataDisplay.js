da36779cc3d719afdf79b61114f5552a94802613
lifei <359952175@qq.com>
2017-10-11 16:25:33 +0800

    1

--- src/js/techPool/enterpriseDataDisplay.js
+++ src/js/techPool/enterpriseDataDisplay.js
@@ -1,4 +1,10 @@
 $(function () {
+
+    $('#nav-classify li').click(function () {
+        $('#nav-classify li').removeClass('active');
+        $(this).addClass('active');
+    });
+
     var data1 = [];
     for(var j = 0; j < 10; j++){
         data1.push(j*10);
@@ -53,8 +59,122 @@ $(function () {
     chart.setOption(option);
 
     $('#table_investOut').bootstrapTable({
-        pagination: true,
-        pageNumber: 1,
-        pageSize: 10
+        columns: [
+            {
+                field:'name',
+                align:"center",
+                valign: "middle",
+                title : "被投资企业名称"
+            },{
+                field : 'person',
+                align:"center",
+                valign: "middle",
+                title : "被投资法定代表人"
+            },{
+                field : 'registerMoney',
+                align:"center",
+                valign: "middle",
+                title : "注册资本"
+            },{
+                field : 'investMoney',
+                align:"center",
+                valign: "middle",
+                title : "投资数额"
+            },{
+                field : 'percent',
+                align:"center",
+                valign: "middle",
+                title : "投资占比"
+            }, {
+                field : 'datetime',
+                align:"center",
+                valign: "middle",
+                title : "注册时间"
+            }, {
+                field : 'state',
+                align:"center",
+                valign: "middle",
+                title : "状态",
+                formatter: function (val) {
+                    if (val == 1){
+                        return '<span class="gray">注销</span>';
+                    }else {
+                        return '<span class="blue">存续</span>';
+                    }
+                }
+            }
+        ],
+        paginationPreText:"<i class='glyphicon glyphicon-menu-left'></i>",
+        paginationNextText:"<i class='glyphicon glyphicon-menu-right'></i>",
+        method: 'get',
+        sortable:true,
+        sortOrder : 'desc',
+        pagination : true,
+        sidePagination : 'server',
+        pageNumber :1,
+        pageSize :10,
+        dataType: "json",
+        url : '../../json/invest.json',
+        pageList : [],
+        paginationDetailHAlign:'left',
+        queryParamsType: '',
+        onLoadSuccess:function(){
+        }
+    });
+
+
+    $('#table_brand_info').bootstrapTable({
+        columns: [
+            {
+                field:'datetime',
+                align:"center",
+                valign: "middle",
+                title : "申请日期"
+            },{
+                field : 'brand',
+                align:"center",
+                valign: "middle",
+                title : "商标"
+            },{
+                field : 'brandName',
+                align:"center",
+                valign: "middle",
+                title : "商标名称"
+            },{
+                field : 'registerNum',
+                align:"center",
+                valign: "middle",
+                title : "注册号"
+            },{
+                field : 'category',
+                align:"center",
+                valign: "middle",
+                title : "类别"
+            }, {
+                field : 'state',
+                align:"center",
+                valign: "middle",
+                title : "状态",
+                formatter: function (val) {
+                    return '';
+                }
+            }
+        ],
+        paginationPreText:"<i class='glyphicon glyphicon-menu-left'></i>",
+        paginationNextText:"<i class='glyphicon glyphicon-menu-right'></i>",
+        method: 'get',
+        sortable:true,
+        sortOrder : 'desc',
+        pagination : true,
+        sidePagination : 'server',
+        pageNumber :1,
+        pageSize :10,
+        dataType: "json",
+        url : '../../json/brand.json',
+        pageList : [],
+        paginationDetailHAlign:'left',
+        queryParamsType: '',
+        onLoadSuccess:function(){
+        }
     });
 });
